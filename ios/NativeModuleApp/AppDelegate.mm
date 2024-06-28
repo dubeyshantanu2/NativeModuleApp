fded70569dd82ff7b4b7b98476d5ea014e541c35
Shantanu Dubey <shantanu@founderandlightning.com>
2024-06-28 16:57:10 +0530

    Register the Module

--- ios/NativeModuleApp/AppDelegate.mm
+++ ios/NativeModuleApp/AppDelegate.mm
@@ -1,31 +1,32 @@
 #import "AppDelegate.h"
-
-#import <React/RCTBundleURLProvider.h>
+#import "React/RCTBridge.h"
+#import "React/RCTBundleURLProvider.h"
+#import "React/RCTRootView.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"NativeModuleApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"NativeModuleApp" initialProperties:nil];
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    rootView.backgroundColor = [UIColor whiteColor];
+
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self bundleURL];
-}
-
-- (NSURL *)bundleURL
-{
-#if DEBUG
+    #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+    #else
+        return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    #endif
 }
 
 @end
